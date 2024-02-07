d1c0f7cf77e5294b795d9f3b95293940ddd0abdb
Hnin Myat Moe Oo <40667676@live.napier.ac.uk>
2024-02-07 14:32:23 +0630

    for population in each continent

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,10 @@
+
 package com.napier.sem;
 
+import java.math.BigDecimal;
 import java.sql.*;
+import java.util.ArrayList;
+//adding necessary libraries, modules
 
 public class App
 {
@@ -8,20 +12,31 @@ public class App
     {
         // Create new Application
         App a = new App();
-
         // Connect to database
         a.connect();
+
+        // Retrieve population in continent details
+        ArrayList<Population> population = a.getPopulation();
+
+        //Retrieve population in region details
+//        ArrayList<Continent> continent = a.getContinent();
+
+        // Display result
+        a.displayPopulation(population);
+//        a.displayContinent(continent);
+
         // Disconnect from database
-//        a.disconnect();
+        a.disconnect();
+
 
     }
     /**
-     * Connection to MySQL database.
+            * Connection to MySQL database.
      */
     private Connection con = null;
 
     /**
-     * Connect to the MySQL database.
+            * Connect to the MySQL database.
      */
     public void connect()
     {
@@ -46,7 +61,8 @@ public class App
                 Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
+//                con = DriverManager.getConnection("jdbc:mysql://localhost:33060/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected jgghkgu");
                 break;
             }
             catch (SQLException sqle)
@@ -62,7 +78,7 @@ public class App
     }
 
     /**
-     * Disconnect from the MySQL database.
+        * Disconnect from the MySQL database.
      */
     public void disconnect()
     {
@@ -79,5 +95,87 @@ public class App
             }
         }
     }
+
+    /**
+     * Population of people who living in cities and people who not living in cities in each continent
+     * */
+    public ArrayList<Population> getPopulation()
+    {
+        ArrayList<Population> a = new ArrayList<Population>();
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    " SELECT " +
+                            " country.Continent," +
+                            " SUM(country.Population) AS Total_Population," +
+                            " SUM(city.Population) AS Population_In_Cities," +
+                            " SUM(country.Population) - SUM(city.Population) AS Population_Not_In_Cities," +
+                            " ROUND((SUM(city.Population) / SUM(country.Population)) * 100, 2) AS Percentage_Population_In_Cities," +
+                            " ROUND(((SUM(country.Population) - SUM(city.Population)) / SUM(country.Population)) * 100, 2) " +
+                            " AS Percentage_Population_Not_In_Cities " +
+                            " FROM " +
+                            " country " +
+                            "INNER JOIN " +
+                            "    city ON country.Code = city.CountryCode\n" +
+                            "GROUP BY " +
+                            "    country.Continent ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            while (rset.next())
+            {
+                Population pol = new Population();
+//                pol.setContinent(rset.getString("Continent"));
+//                pol.setTotal_Population(rset.getInt("TotalPopulation"));
+//                pol.setPopulation_In_Cities(rset.getInt("PopulationInCities"));
+//                pol.setPopulation_Not_In_Cities(rset.getInt("PopulationNotInCities"));
+//                pol.setPercentage_Population_In_Cities(new BigDecimal(rset.getString("PercentagePopulationInCities")));
+//                pol.setPercentage_Population_Not_In_Cities(new BigDecimal(rset.getString("PercentagePopulationNotInCities")));
+                // Change the column names in your Java code to match the ones in the SQL query
+                pol.setContinent(rset.getString("Continent"));
+                pol.setTotal_Population(rset.getLong("Total_Population"));
+                pol.setPopulation_In_Cities(rset.getLong("Population_In_Cities"));
+                pol.setPopulation_Not_In_Cities(rset.getLong("Population_Not_In_Cities"));
+                pol.setPercentage_Population_In_Cities(new BigDecimal(rset.getString("Percentage_Population_In_Cities")));
+                pol.setPercentage_Population_Not_In_Cities(new BigDecimal(rset.getString("Percentage_Population_Not_In_Cities")));
+
+                // Check for null value before converting to BigDecimal
+                a.add(pol);
+            }
+            return a;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population in each continent details");
+            return null;
+        }
+    }
+    /**
+        * Display population details.
+     */
+    public void displayPopulation(ArrayList<Population> populations) {
+        if (populations != null && !populations.isEmpty()) {
+            System.out.println("------------------------------------------------------------------------------------------------------------------------");
+            System.out.printf("| %-25s | %-40s | %-40s | %-40s | %-40s | %-40s |\n",
+                    "Continent", "TotalPopulation", "PopulationInCities", "PopulationNotInCities", "PercentagePopulationInCities", "PercentagePopulationNotInCities");
+            System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+
+            for (Population pol : populations) {
+                System.out.printf("| %-25s | %-40s | %-40s | %-40s | %-40s | %-40s |\n",
+                        pol.getContinent(), pol.getTotal_Population(),pol.getPopulation_In_Cities(), pol.getPopulation_Not_In_Cities(), pol.getPercentage_Population_In_Cities(), pol.getPercentage_Population_Not_In_Cities());
+            }
+
+            System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------");
+        } else {
+            System.out.println("No population details available");
+        }
+    }
+
+
 
 }
---
+++ src/main/java/com/napier/sem/Population.java
@@ -0,0 +1,85 @@
+package com.napier.sem;
+
+import java.math.BigDecimal;
+
+/**
+ * Represents a countries
+ */
+public class Population
+{
+
+    /**
+     * Country continent
+     */
+    private String Continent;
+    /**
+     * Continent
+     */
+    private long Total_Population;
+    /**
+     * Total_Population
+     */
+    private long Population_In_Cities;
+    /**
+     * Population In Cities
+     */
+    private long Population_Not_In_Cities;
+    /**
+     * Population Not In Cities
+     */
+    private BigDecimal Percentage_Population_In_Cities;
+    /**
+     * Percentage Population In Cities
+     */
+    private BigDecimal Percentage_Population_Not_In_Cities;
+    /**
+     * Percentage Population Not In Cities
+     */
+    public String getContinent() {
+        return Continent;
+    }
+
+    public void setContinent(String continent) {
+        Continent = continent;
+    }
+
+    public long getTotal_Population() {
+        return Total_Population;
+    }
+
+    public void setTotal_Population(long total_Population) {
+        Total_Population = total_Population;
+    }
+
+    public long getPopulation_In_Cities() {
+        return Population_In_Cities;
+    }
+
+    public void setPopulation_In_Cities(long population_In_Cities) {
+        Population_In_Cities = population_In_Cities;
+    }
+
+    public long getPopulation_Not_In_Cities() {
+        return Population_Not_In_Cities;
+    }
+
+    public void setPopulation_Not_In_Cities(long population_Not_In_Cities) {
+        Population_Not_In_Cities = population_Not_In_Cities;
+    }
+
+    public BigDecimal getPercentage_Population_In_Cities() {
+        return Percentage_Population_In_Cities;
+    }
+
+    public void setPercentage_Population_In_Cities(BigDecimal percentage_Population_In_Cities) {
+        Percentage_Population_In_Cities = percentage_Population_In_Cities;
+    }
+
+    public BigDecimal getPercentage_Population_Not_In_Cities() {
+        return Percentage_Population_Not_In_Cities;
+    }
+
+    public void setPercentage_Population_Not_In_Cities(BigDecimal percentage_Population_Not_In_Cities) {
+        Percentage_Population_Not_In_Cities = percentage_Population_Not_In_Cities;
+    }
+}
